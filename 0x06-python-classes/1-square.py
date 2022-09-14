39199ff082c607322c5822621ec5756c595912e7
mike-nyange <micknyange@gmail.com>
2022-09-14 15:11:40 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square by size"""
+
+
+class Square:
+    """Initiating an instance of the class"""
+
+    def __init__(self, size=0):
+        self.size = size
