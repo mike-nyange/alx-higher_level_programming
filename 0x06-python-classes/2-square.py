2602bdae6022672ece33504bfe306ffacb44495b
mike-nyange <micknyange@gmail.com>
2022-09-14 15:45:30 +0300

    task 2 final commit hopefully

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     """Initiating an instance of the class"""
 
     def __init__(self, size=0):
-        if (type(size) == int):
+        if not (type(size) == int):
             raise TypeError('size must be an integer')
-        elif(size < 0):
+        if(size < 0):
             raise ValueError('size must be >= 0')
-            
+        self.__size = size
