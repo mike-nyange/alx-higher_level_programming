e91b7616db59666bb48d550f8b047b92c84bf495
mike-nyange <micknyange@gmail.com>
2022-09-14 15:50:05 +0300

    3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square by size"""
+
+
+class Square:
+    """Initiating an instance of the class"""
+
+    def __init__(self, size=0):
+        if not (type(size) == int):
+            raise TypeError('size must be an integer')
+        if(size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
