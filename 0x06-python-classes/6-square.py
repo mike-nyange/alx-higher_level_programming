1e1580978992df38073ddf168213341157c45e51
mike-nyange <micknyange@gmail.com>
2022-09-14 21:32:53 +0300

    6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,12 +6,13 @@
 class Square:
     """Initiating an instance of the class"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         if not (type(size) == int):
             raise TypeError('size must be an integer')
         if(size < 0):
             raise ValueError('size must be >= 0')
         self.__size = size
+        self.__position = position
 
     def area(self):
         area = self.__size * self.__size
@@ -28,11 +29,21 @@ class Square:
         if(value < 0):
             raise ValueError('size must be >= 0')
         self.__size = value
-        
-        
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def size(self, value):
+        if not (type(value) == int):
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+        self.position = (value, value)
+
     def my_print(self):
-        
+
         for i in range(0, self.__size):
             for j in range(0, self.__size):
-                print("#", end="\n" if j is self.__size -1 and i != j else "")
+                print("#", end="\n" if j is self.__size - 1 and i != j else "")
         print()
