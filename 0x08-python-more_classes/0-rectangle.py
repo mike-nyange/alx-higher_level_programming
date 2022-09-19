3287b4d1da2cdcef8baec1814557319c166f70d3
mike-nyange <micknyange@gmail.com>
2022-09-19 21:28:17 +0300

    task 0 correction

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,5 +2,7 @@
 ''''Task:Write an empty class Rectangle that defines a rectangle'''
 
 
-class Rectangle:
+class Rectangle():
+    '''This is a class that creates instances of a
+    rectangle'''
     pass
