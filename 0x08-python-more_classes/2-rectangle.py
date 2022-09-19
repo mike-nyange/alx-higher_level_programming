b214b14050ec1914ffd89ee7d10fba834e774621
mike-nyange <micknyange@gmail.com>
2022-09-19 22:14:28 +0300

    task 2 initial commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+''''Task:Write an empty class Rectangle that defines a rectangle'''
+
+
+class Rectangle():
+    '''This is a class that creates instances of a
+    rectangle'''
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    def area(self):
+        area = (self.__width * self.__height)
+        return area
+
+    def perimeter(self):
+        perimeter = ((self.__width * 2) + (self.__height * 2))
+        return perimeter
