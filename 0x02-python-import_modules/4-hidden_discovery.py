219e4f47f88af28960a1ff4cf44e1e30c161bca1
mike-nyange <micknyange@gmail.com>
2022-11-08 11:10:28 +0300

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    all_dir = dir(hidden_4)
-    for i in range(0, len(all_dir)):
-        if "__" != all_dir[i][:2]:
-            print(all_dir[i])
+    # all_dir = dir(hidden_4)
+    # for i in range(0, len(all_dir)):
+    #     if "__" != all_dir[i][:2]:
+    #         print(all_dir[i])
+    for arg in dir(hidden_4):
+        if arg[:2] != "__":
+            print("{}".format(arg))
