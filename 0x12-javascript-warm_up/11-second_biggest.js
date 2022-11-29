a24ed90f5ef414433ceb1ad785007240988f78ea
mike-nyange <micknyange@gmail.com>
2022-11-29 22:05:47 +0300

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,33 @@
 #!/usr/bin/node
 
-function second (myArray) {
-  if (myArray.length === 2 || myArray.length === 3) { return (0); }
+let args = process.argv.slice(2).map((x) => {
+  return parseInt(x);
+});
 
-  let max = myArray[2];
-  let secondMax = myArray[3];
-
-  for (let i = 2; i < myArray.length; i++) {
-    if (myArray[i] > max) {
-      secondMax = max;
-      max = myArray[i];
-    } else if (myArray[i] > secondMax && myArray[i] < max) {
-      secondMax = myArray[i];
-    }
-  }
-  return (secondMax);
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  console.log(args.sort((a, b) => {
+    return b - a;
+  })[1]);
 }
+// #!/usr/bin/node
 
-console.log(second(process.argv));
+// function second (myArray) {
+//   if (myArray.length === 2 || myArray.length === 3) { return (0); }
+
+//   let max = myArray[2];
+//   let secondMax = myArray[3];
+
+//   for (let i = 2; i < myArray.length; i++) {
+//     if (myArray[i] > max) {
+//       secondMax = max;
+//       max = myArray[i];
+//     } else if (myArray[i] > secondMax && myArray[i] < max) {
+//       secondMax = myArray[i];
+//     }
+//   }
+//   return (secondMax);
+// }
+
+// console.log(second(process.argv));
